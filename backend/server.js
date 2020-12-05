ba89c4608aae8ff61f9d86997c47f80b68d5bb5d
Nikhil Devadoss <nikdev@Nikhils-MBP.hsd1.ca.comcast.net>
2020-12-05 15:17:34 -0800

    setup backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+//mongodb credentials
+//admin
+//Chinmay123!
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://admin:Chinmay123!@cluster0.9zhva.mongodb.net/rank_db?retryWrites=true&w=majority', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
